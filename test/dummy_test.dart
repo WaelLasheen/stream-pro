61eba79721f91877ff5cdbc7b9c87117ab18ddfd
Wael Lasheen <lasheenwael9@gmail.com>
2026-08-26 09:52:05 +0300

    O_O

---
+++ test/dummy_test.dart
@@ -0,0 +1,9 @@
+// just empty file for test github actions
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Initial dummy test', () {
+    expect(true, isTrue);
+  });
+}
